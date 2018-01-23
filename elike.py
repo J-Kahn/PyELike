248c0880d6ff13c8ad71a486375e17f69b72fa85
J-Kahn <rjaykahn@umich.edu>
2018-01-23 11:26:55 -0500

    Update to GMM estimation.

--- elike.py
+++ elike.py
@@ -3,6 +3,96 @@
 import numpy as np
 import scipy.optimize as opt
 
+from numpy import *
+from math import *
+
+#	 Numerical Jacobian using Richardson extrapolation
+#	 Jay Kahn - University of Rochester, November 19, 2012
+#	 f - function to take derivative over
+#	 x - point (vector) at which to evaluate derivatives
+#	 o - order of error term desired relative to f
+#	 h1 - starting factor
+#	 *control - any extra arguments to be passed to f
+def richardson(f, x0, o, h1, v, *control):
+	x=np.array(x0)
+	d=x.shape[0]
+	i=1
+	r=o/2
+	while i <= d:
+		j=1
+		while j <= r:
+			if j==1:
+				h=h1
+			else:
+				h=h/v
+			
+			idd=np.eye(d)*h
+			xup=x+idd[:,i-1]
+			xdown=x-idd[:,i-1]
+			fat=f(x,*control)
+			fup=f(xup,*control)
+			fdown=f(xdown,*control)
+			ddu=fup-fat
+			ddd=fdown-fat
+			
+			hp=h
+			
+			if j==1:
+				dds=np.array([ddu, ddd])
+				hhs=np.array([[hp, -hp]])
+				
+			else:
+				dds=concatenate((dds, np.array([ddu, ddd])),0)
+				hhs=concatenate((hhs, np.array([[hp, -hp]])),1)
+				
+			j=j+1
+			
+		mat=hhs
+		
+		j=2
+		
+		while j<=o:
+			mat=np.concatenate((mat, power(hhs,j)/factorial(j)),0)
+			j=j+1
+		
+		mat
+		der= np.dot(np.transpose(np.linalg.inv(mat)),dds)
+		
+		
+		if i==1:
+			g=der
+		else:
+			g=np.concatenate((g,der),1)
+		
+		i=i+1
+	return g
+
+#	 Jacobian running as shell of Richardson. Ends up with matrix
+#	 whose rows are derivatives with respect to different elements
+#	 of x and columns are derivatives of different elements of f(x).
+#	 For scalar valued f(x) simplifies to column gradient.
+#	 Jay Kahn - University of Rochester, November 19, 2012
+#	 f - function to take derivative over
+#	 x - point (vector) at which to evaluate derivatives
+#	 o - order of error term desired relative to f
+#	 h1 - starting factor
+#	 *control - any extra arguments to be passed to f
+def jacobian(f, x0, o=4, h1=0.5, v=2, *control):
+	fn=f(x0,*control).shape[0]
+	x=np.array(x0)
+	xn=x.shape[0]
+	J=np.zeros((xn,fn))
+	g=richardson(f, x, o, h1, v, *control)
+	j=0
+	while j<=xn-1:
+		i=0
+		while i<=fn-1:
+			J[j,i]=g[0,i+j*fn]
+			i=i+1
+		j=j+1
+	return J.T
+
+
 def cueobj(x):
 	return x**2
 
@@ -79,20 +169,34 @@ class elspec:
 		self.estim = opt.minimize(self.gel_obj, initpar, method = 'Nelder-Mead')
 		self.theta = self.estim.x
 
-	def gmm_estimate(self, initpar):
+	def gmm_iteration(self, initpar, method = 'Nelder-Mead', minimizer_kwargs = {}):
+		if method == 'basinhopping':
+			self.estim = opt.basinhopping(self.gmm_obj, initpar, minimizer_kwargs = {})
+		else:
+			self.estim = opt.minimize(self.gmm_obj, initpar, method = method)
+		par = self.estim.x
+		        
+		return par
+    
+	def gmm_initiate(self, initpar):
 		self.mv(initpar)
 		self.np = len(initpar)
 		self.nm = self.res.shape[1]
 		self.mdiff = 50
-		self.W = np.identity(self.nm) 
-
+		self.W = np.identity(self.nm)         
+        
+	def gmm_estimate(self, initpar, method = 'Nelder-Mead', maxit = 5000, minimizer_kwargs = {}):
+		self.gmm_initiate(initpar)
+		it = 0
 		while self.mdiff > 1e-6:
-			self.estim = opt.minimize(self.gmm_obj, initpar, method = 'Nelder-Mead')
-			par = self.estim.x
-			self.update_weight(par)
-			self.mdiff = np.max((self.estim.x - initpar)**2)
+			
+			par = self.gmm_iteration(initpar, method = method, minimizer_kwargs = minimizer_kwargs)
+			self.update_weight(par)   
+			self.mdiff = np.max((par - initpar)**2)
 			print(self.mdiff)
-			initpar = self.estim.x
+			if it > maxit:
+				self.mdiff = 0
+			#initpar = par
 
 		self.theta = par
 
@@ -100,10 +204,42 @@ class elspec:
 		self.mv(par)
 		moe = np.mean(self.res, axis = 0)
 		return np.dot(np.dot(moe.T, self.W), moe)
+    
+	def gmm_var(self, par, efficient = False):
+		G = jacobian(self.mvm,self.theta,4,0.5,2)
+		gwginv = np.linalg.inv(np.dot(np.dot(G.T, self.W), G))
+		if efficient:
+			self.var = gwginv / float(self.data.shape[0])
+		else:
+			omega = self.get_var(par)
+			gwowg = np.dot(np.dot(G.T, self.W), omega)
+			gwowg = np.dot(gwowg, np.dot(self.W.T, G))
+			self.var = np.dot(np.dot(gwginv, gwowg), gwginv)/float(self.data.shape[0])
+
+	def gmm_jstat(self, par, efficient = False):
+		if efficient:
+			psi = np.linalg.inv(self.W)
+			invpsi = self.W
+		else:
+			omega = self.get_var(par)
+			G = jacobian(self.mvm,self.theta,4,0.5,2)
+			peye = np.eye(G.shape[0])
+			gwginv = np.linalg.inv(np.dot(np.dot(G.T, self.W), G))
+			gwc = np.dot(np.dot(np.dot(G, gwginv), G.T), self.W)
+			psi = np.dot(np.dot(peye - gwc, omega), (peye - gwc).T)
+			invpsi = np.linalg.pinv(psi)
+		self.psi = psi / float(self.data.shape[0])
+		m = self.mvm(par)
+		self.J = np.dot(np.dot(m.transpose(), invpsi), m) * float(self.data.shape[0])
 
 	def update_weight(self, par):
 		self.mv(par)
-		self.W = np.linalg.inv(np.dot(self.res.T,self.res))/float(self.data.shape[0])
+		self.W = np.linalg.inv(np.dot(self.res.T,self.res)/float(self.data.shape[0]))
+
+	def get_var(self, par):
+		self.mv(par)
+		omega= np.dot(self.res.T,self.res)/float(self.data.shape[0])
+		return omega
 
 	def gel_obj(self, par):
 
@@ -133,6 +269,10 @@ class elspec:
 	def mv(self, par):
 
 		self.res = self.moments(par, self.data)
+
+	def mvm(self, par):
+
+		return np.mean(self.moments(par, self.data),0)
 
 	def om(self, par):
 		return self.obj(np.dot(self.res, self.lagrange))
